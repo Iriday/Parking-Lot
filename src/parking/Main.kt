f21ee7d4670cae4e45e8690199b54d0ebdd9a0ca
Igor <GI_Something@protonmail.com>
2020-03-23 20:17:29 +0200

    fix: Output err message if user input is incorrect (added input checks)

--- src/parking/Main.kt
+++ src/parking/Main.kt
@@ -9,7 +9,7 @@ fun main() {
 
 class Main {
     private val scn = Scanner(System.`in`)
-    private lateinit var spots: Array<Spot>
+    private var spots: Array<Spot> = arrayOf()
     private var on = true
     private var created = false // parking lot
 
@@ -27,8 +27,9 @@ class Main {
                 continue
             }
 
-            if (!created && !(command == CREATE || command == EXIT)) {
-                output("Parking lot is not created.")
+            val errMessage = inputCheck(input, command, spots)
+            if (errMessage.isNotEmpty()) { // incorrect input
+                output(errMessage)
                 continue
             }
 
@@ -40,13 +41,10 @@ class Main {
         return when (command) {
             CREATE -> {
                 val size = input[1].toInt()
-                if (size < 1) "Incorrect input."
-                else {
-                    var i = 0 // supplier
-                    spots = Array(size) { Spot((++i).toString()) }
-                    created = true
-                    "Created a parking lot with $size spots."
-                }
+                var i = 0 // supplier
+                spots = Array(size) { Spot((++i).toString()) }
+                created = true
+                "Created a parking lot with $size spots."
             }
             PARK -> {
                 val spotIdentifier = park(Car(input[1], input[2]), spots)
@@ -57,7 +55,7 @@ class Main {
                 val unitLeft = leave(input[1], spots)
                 if (unitLeft == true) "Spot ${input[1]} is free."
                 else if (unitLeft == false) "There is no car in the spot ${input[1]}."
-                else "Incorrect input"
+                else "Error, incorrect parking spot identifier."
             }
             STATUS -> {
                 val out = status(spots)
@@ -85,10 +83,56 @@ class Main {
         }
     }
 
+    private fun inputCheck(input: List<String>, command: Command, spots: Array<Spot>): String {
+        val inputSize = input.size
+
+        // check input size
+        val correctSize = when (command) {
+            EXIT, STATUS -> inputSize == 1
+            CREATE, LEAVE, REG_BY_COLOR, SPOT_BY_COLOR, SPOT_BY_REG -> inputSize == 2
+            PARK -> inputSize == 3
+        }
+        if (!correctSize) return "Error, incorrect number of arguments."
+
+        //
+        if (!created && !(command == CREATE || command == EXIT)) {
+            return "Parking lot is not created."
+        }
+
+        // check casts
+        val correctCasts = when (command) {
+            CREATE -> input[1].toIntOrNull() != null
+            else -> true
+        }
+        if (!correctCasts) return "Error, incorrect input."
+
+        // check special cases
+        val errSpecialCases: String = when (command) {
+            CREATE -> if (input[1].toInt() < 1) "Error, incorrect value." else emptyStr
+            // check car number
+            PARK -> {
+                var out = emptyStr
+                for (spot in spots) {
+                    if (spot.car?.number == input[1]) {
+                        out = "Error, the car with the number ${input[1]} already parked."
+                        break
+                    }
+                }
+                out
+            }
+            else -> emptyStr
+        }
+        if (errSpecialCases.isNotEmpty()) return errSpecialCases
+
+        return emptyStr // correct input
+    }
+
     private fun input(): List<String> = scn.nextLine().trim().split(Regex(" "))
 
     private fun output(str: String) = println(str)
 }
+
+private const val emptyStr = ""
 
 fun park(car: Car, spots: Array<Spot>): String? {
     for (i in spots.indices) {
